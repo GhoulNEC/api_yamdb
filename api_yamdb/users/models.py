4e5ac19cc391382ee4767a677c132f12d57f09ba
Вячселав Наприенко <Hellon048@gmail.com>
2022-07-12 02:28:03 +0500

    model.user and validators

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -16,15 +16,6 @@ class User(AbstractUser):
         (ADMIN, ADMIN),
     )
     username_validator = UsernameValidator()
-    username = models.CharField(
-        'Имя пользователя',
-        max_length=150,
-        unique=True,
-        validators=[username_validator],
-    )
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    email = models.EmailField('Email', max_length=254, unique=True)
     role = models.CharField(
         'Роль пользователя',
         choices=roles,
@@ -36,9 +27,6 @@ class User(AbstractUser):
         max_length=100,
         null=True
     )
-
-    REQUIRED_FIELDS = ['email']
-    USERNAME_FIELDS = 'email'
 
     def __str__(self):
         return str(self.username)
